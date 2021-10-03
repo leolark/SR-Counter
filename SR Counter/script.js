2993b893525ce0e0d34c5ee5d8521b2d177b66ec
Leonte Zhavoronkov <32207706+leolark@users.noreply.github.com>
2021-10-03 23:04:37 +0300

    Add files via upload

--- SR Counter/script.js
+++ SR Counter/script.js
@@ -1,16 +1,48 @@
-let resultPercent = document.querySelector('.counter-value-percent');
+"use strict"
 
-let geometryOriginal = document.querySelector('.geometry');
-let labelsOriginal = document.querySelector('.labels');
-let classesOriginal = document.querySelector('.classes');
-let operatorOriginal = document.querySelector('.operator');
+const allInputs = document.querySelectorAll('input');
+const resultElement = document.querySelector('.counter-value-percent');
 
-document.oninput = function() {
-    let geometry = geometryOriginal.value * 100 / 12000;
-    let labels = labelsOriginal.value * 100 / 11000;
-    let classes = classesOriginal.value * 100 / 5000;
-    let operator = operatorOriginal.value * 100 / 700;
-    let result = geometry + labels + classes + operator;
-    result = result.toFixed(2);
-    resultPercent.innerHTML = result + '%';
-};
+const inputValues = {
+    geometry: 0,
+    labels: 0,
+    classes: 0,
+    operator: 0
+}
+
+const onChangeHandler = (event) => {
+    // при каждом изм значения в инпуте, меняем ключи в объекта inputValues
+    inputValues[event.target.name] = +event.target.value
+    
+    // достаем деструктуризацией знчения ключей объекта
+    const {geometry, labels, classes, operator} = inputValues
+
+    const result = (geometry * 100 / 12000) 
+    + (labels * 100 / 11000) 
+    + (classes * 100 / 5000) 
+    + (operator * 100 / 700);
+
+    resultElement.innerHTML = `${result.toFixed(2)}%`
+}
+
+const keyboardEventHandler = (event) => {
+    // деструктуризация объектов/массивов как работает почитать
+    const {keyCode, target: {value, max}} = event;  
+    // const isValidValue = !value || (+value <= +max);
+
+    const isBeginWithZero = (!value && keyCode === 48) 
+    || (!value && keyCode === 96)
+    
+    const isValidKey = (keyCode >= 48 && keyCode <= 57) 
+    || (keyCode >= 96 && keyCode <= 105)
+    || (keyCode === 8)
+
+    if (!isValidKey || isBeginWithZero) {
+        event.preventDefault()
+    }
+}
+
+allInputs.forEach((input) => {
+    input.addEventListener('input', onChangeHandler)
+    input.addEventListener('keydown', keyboardEventHandler)
+})
